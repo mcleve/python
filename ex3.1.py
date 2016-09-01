3e4a9e5c832be2da081f3fcbf67771f257a40879
mcleve <matthewjamescleveland@gmail.com>
2016-08-31 22:50:49 -0500

    example 3 with comments

---
+++ ex3.1.py
@@ -0,0 +1,38 @@
+# Print the following line
+print "I will now count my chickens:"
+
+# Print Hens and divid 30/6 then add 25 to result.
+print "Hens", 25 + 30 /6
+# Print Roosters and the (25 * 3)= 75, (75 % 4)=3 this is the remainder of (75 / 4)=3 so 100 - 3 is 97  
+print "Roosters", 100 - 25 * 3 % 4
+
+# print the following line
+print "Now I will count the eggs:"
+
+# 3+2+1-5+(4%2)-(1/4)+6
+print 3 + 2 +1 - 5 + 4 % 2 -1 / 4 + 6
+
+# print following line
+print "Is it true the 3 + 2 < 5 - 7?"
+
+# evaluate 3+2 < 5-7 return false
+print 3 + 2 < 5 - 7
+
+# print line and result of 3+2
+print "What is 3 + 2?", 3 + 2
+# print line and result of 5-7
+print "What is 5 - 7?", 5-7
+
+# print following line
+print "Oh, that's why it's False."
+
+# print following line
+print "How about some more."
+
+# print line and evaluate if 5 > -2
+print "Is it greater?", 5 > -2
+# print line and evaluate if 5 >= -2
+print "Is it greater or equal?", 5 >= -2
+# print line and evaluate if 5 <= -2
+print "Is it less or equal?", 5 <= -2
+
